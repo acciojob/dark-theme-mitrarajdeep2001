242c35b6607eb006223a430d98c178087b9e3def
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-11-08 18:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+const btn = document.querySelector("#swap")
+function toggleTheme() {
+	const app = document.querySelector("#app")
+	app.classList.toggle("night")
+	btn.classList.toggle("button_night")
+}
 
+btn.addEventListener("click", toggleTheme)
